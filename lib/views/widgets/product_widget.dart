ce76482780e4b385896774d091e82d47bdf75977
Bakhodir <106083101+Bakhodir95@users.noreply.github.com>
2024-05-29 22:57:44 +0500

    qaws

--- lib/views/widgets/product_widget.dart
+++ lib/views/widgets/product_widget.dart
@@ -8,46 +8,50 @@ class ProductWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width / 2 - 15,
-      child: Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: Container(
-                clipBehavior: Clip.hardEdge,
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(20)),
-                width: MediaQuery.of(context).size.width / 3 - 50,
-                child: Image.network(
-                  product.imagepath,
-                  fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Container(
+        width: MediaQuery.of(context).size.width / 2 - 15,
+        child: Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(20)),
+                  width: MediaQuery.of(context).size.width / 3 - 50,
+                  child: Image.network(
+                    product.imagepath,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            const Gap(10),
-            Text(
-              "\$${product.price}",
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-            ),
-            const Gap(5),
-            Text(
-              product.title,
-              style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black),
-            ),
-            const Gap(5),
-            Text(
-              product.name,
-              style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black),
-            ),
-          ],
+              const Gap(10),
+              Text(
+                "\$${product.price}",
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+              ),
+              const Gap(5),
+              Text(
+                product.title,
+                style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              const Gap(5),
+              Text(
+                product.name,
+                style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ],
+          ),
         ),
       ),
     );
